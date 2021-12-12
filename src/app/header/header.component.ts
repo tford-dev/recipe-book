2aa6e3d3a87d2ff5c16884d78c4341bb3f2426fc
tford-dev <terranceford513@gmail.com>
2021-12-12 16:41:16 -0500

    Added data binding to components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  //styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+  }
+
+  // collapsed = false;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,6 +9,7 @@ import {Recipe} from '../recipe.model';
 })
 
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
@@ -15,5 +17,9 @@ export class RecipeListComponent implements OnInit {
 
     constructor(){}
     ngOnInit(){
+    }
+
+    onRecipeSelected(recipe: Recipe){
+        this.recipeWasSelected.emit(recipe);
     }
 }
